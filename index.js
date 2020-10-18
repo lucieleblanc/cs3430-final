361cb2040ad4540a45e960f65710c81189dc20a0
lucieleblanc <lucieleblanc425@gmail.com>
2020-10-18 18:21:28 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const MINOR = "m"
+const MAJOR = "M"
+
+
+function getNeighbors(root, quality) {
+	
+	let newQuality
+	if (quality == MINOR) {
+		newQuality = MAJOR
+	} else {
+		newQuality = MINOR
+	}
+
+	let newNotes = []
+	for (var i = 0; i < 3; i++) {
+		
+	}
+}
+
+
